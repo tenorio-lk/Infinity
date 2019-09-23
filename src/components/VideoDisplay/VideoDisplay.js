e146035d01b77fec6ed442617d54f6c1180a854e
Tenorio <diegotenorio88@gmail.com>
2019-09-22 23:43:10 -0600

    Added VideoDisplay page with the basic video information / comments left to do

--- src/components/VideoDisplay/VideoDisplay.js
+++ src/components/VideoDisplay/VideoDisplay.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Layout from '../Layout/Layout'
+import VideoPlayer from '../VideoPlayer/VideoPlayer'
+import { basicData } from '../../helper/AuthAPI/AuthAPI'
+import Aux from '../../hoc/Aux/Aux'
 
-const videoDisplay = ({ match : { params } }) => (
-    <h1>Now displaying video with the ID: {params.videoId}</h1>
-)
+const SUCCESFUL = 200
+
+class videoDisplay extends Component {
+    state = { videoInfo: null }
+
+    componentDidMount = async () => {
+        const { match : { params } } = this.props
+        const response = await basicData.get('/videos', {
+            params: {
+                id: params.videoId,
+                part: 'snippet, statistics'
+            }
+        })
+
+        if (response.status === SUCCESFUL) {
+            this.setState({ videoInfo: response.data.items[0] })
+        }
+    }
+
+    render() {
+        const { videoInfo } = this.state
+        let player = null
+        if (videoInfo) {
+            player = 
+            <Layout searchInitialValue={videoInfo.snippet.title}>
+                <VideoPlayer videoID={videoInfo.id}/>
+                <h1>Now displaying video with the ID: {videoInfo.id}</h1>
+                <h1>Title: {videoInfo.snippet.title}</h1>
+                <h1>Description: {videoInfo.snippet.description}</h1>
+                <h1>Uploaded at: {new Date(videoInfo.snippet.publishedAt).toLocaleDateString()}</h1>
+                <h1>Chanel: {videoInfo.snippet.channelTitle}</h1>
+                <h2>Views: ({videoInfo.statistics.viewCount}) Likes: ({videoInfo.statistics.likeCount}) Dislikes: ({videoInfo.statistics.dislikeCount})</h2>
+                <h3>Comments Count: ({videoInfo.statistics.commentCount})</h3>
+            </Layout>
+        }
+
+        return (
+            <Aux>
+                {player}
+            </Aux>
+        )
+    }
+}
 
 export default videoDisplay
--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const videoPlayer = (props) => (
-    <iframe width="100%" height="auto" title="Hello" src={`https://www.youtube.com/embed/${props.videoID}`} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen>
-    </iframe>
-)
+const videoPlayer = ({ videoID }) => {
+    let displayIframe = null
+    
+    if (videoID) {
+        displayIframe = <iframe width="100%" height="auto" title="Hello" src={`https://www.youtube.com/embed/${videoID}`} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen />
+    }
+
+    return (
+        <div>
+            {displayIframe}
+        </div>
+    )
+}
 
 export default videoPlayer
