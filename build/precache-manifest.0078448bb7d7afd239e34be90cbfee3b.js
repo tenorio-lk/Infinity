bee0c37e406b9a884c15b1ae1a36fe4c2f3e1f9e
Tenorio <diegotenorio88@gmail.com>
2019-09-25 20:34:45 -0600

    Added build package

---
+++ build/precache-manifest.0078448bb7d7afd239e34be90cbfee3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb7d4c042893449c536120522150737",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c4b8613523998525397",
+    "url": "/static/css/main.e231e84c.chunk.css"
+  },
+  {
+    "revision": "18463b03e467684441ad",
+    "url": "/static/js/2.a91182dc.chunk.js"
+  },
+  {
+    "revision": "3c4b8613523998525397",
+    "url": "/static/js/main.b1c1a86a.chunk.js"
+  },
+  {
+    "revision": "86ee206754c642c7fcf6",
+    "url": "/static/js/runtime~main.3fd402c4.js"
+  }
+]);
